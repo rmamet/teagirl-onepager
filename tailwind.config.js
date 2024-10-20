b80fa93fee82106057bba3f7650ce222d825450a
rmamet <rmamet@rmamet.xyz>
2024-10-19 22:10:14 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        't-white': 'rgba(255, 255, 255, .9)',
+      },
+    },
+  },
+  plugins: [],
+}
